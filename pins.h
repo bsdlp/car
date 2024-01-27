bb8e357eaab6ebf38a114d5e261b5e22b63f78c6
Jon Chen <bsd@voltaire.sh>
2024-01-27 09:14:48 +0800

    define pins in a header file

---
+++ pins.h
@@ -0,0 +1,26 @@
+#ifndef PINS_H
+#define PINS_H
+
+// SPI Defines
+// We are going to use SPI 0, and allocate it to the following GPIO pins
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define SPI_PORT spi0
+#define PIN_MISO 16
+#define PIN_CS 17
+#define PIN_SCK 18
+#define PIN_MOSI 19
+
+// I2C defines
+// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define I2C_PORT i2c0
+#define I2C_SDA 8
+#define I2C_SCL 9
+
+// motor pins
+#define MOTOR_LEFT_FRONT 2
+#define MOTOR_LEFT_REAR 3
+#define MOTOR_RIGHT_FRONT 4
+#define MOTOR_RIGHT_REAR 5
+
+#endif // PINS_H
